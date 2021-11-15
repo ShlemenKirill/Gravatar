73b0ed5c125591cf9e8cd724f5ef24502ca00876
ShlemenKirill <kirill.shlemen@gmail.com>
2021-11-15 13:12:33 +0300

    change to module.exports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import MD5 from "./lib/md5.js";
+const MD5 = require("./lib/md5.js")
 
 const EMAIL_REGEX =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -27,7 +27,9 @@ const gravatarTypes = {
   BLANK: "blank",
 };
 
-export const gravatarURL = {
+const gravatarURL = {
   getGravatarUrl,
   gravatarTypes,
 };
+
+module.exports = gravatarURL;
